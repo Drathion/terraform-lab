5c644906cb0d7aa5ffc2257f40cecd6342aa5041
Drathion <drathion@outlook.com>
2025-02-08 19:04:34 -0500

    add base.tf

---
+++ base.tf
@@ -0,0 +1,42 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.80"
+      }
+    }
+}
+
+provider "aws" {
+    region = "us-east-2"
+    profile = "default"
+}
+
+locals {
+  subnet_blocks = tomap({
+    "a" = "10.10.1.0/24"
+    "b" = "10.10.2.0/24"
+    "c" = "10.10.3.0/24"
+  })
+}
+
+resource "aws_vpc" "practice-vpc" {
+    cidr_block = "10.10.0.0/16"
+
+    tags = {
+      "tf-project" = "terraform-lab"
+      "Name" = "terraform-lab" # Name is the default "AWS Name" tag. Case sensitive
+    }
+}
+
+resource "aws_subnet" "terraform-lab-subnet-private" {
+  for_each = local.subnet_blocks
+  vpc_id = aws_vpc.practice-vpc.id
+  cidr_block = each.value
+
+  tags = {
+    "Name" = each.key
+    "tf-project" = "aws-practice"
+  }
+}
+
